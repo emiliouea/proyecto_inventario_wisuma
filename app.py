b4ec6f0c12917270e43d9fb7b148f3dc87504e71
emiliosdev <sengua2000@outlook.com>
2026-02-15 22:19:33 -0500

    Import os agregado para puerto Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -13,5 +14,5 @@ def item(codigo):
     return f"Item código {codigo} – Disponible en bodega."
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
